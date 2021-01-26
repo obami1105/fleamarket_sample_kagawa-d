2d3e015404da7f49696337c663f5de1ba27c137f
mk299 <dev+github@clover-dot.net>
2021-01-26 23:00:12 +0900

    add: Destinationsテーブル作成

--- db/migrate/20210126135715_create_destinations.rb
+++ db/migrate/20210126135715_create_destinations.rb
@@ -1,7 +1,13 @@
 class CreateDestinations < ActiveRecord::Migration[6.0]
   def change
     create_table :destinations do |t|
-
+      t.integer    :post_code,      null: false
+      t.integer    :prefecture_id,  null: false
+      t.string     :city,           null: false
+      t.string     :house_number,   null: false
+      t.string     :building_name
+      t.string     :phone_number
+      t.references :user,           null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_121429) do
+ActiveRecord::Schema.define(version: 2021_01_26_135715) do
+
+  create_table "destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "post_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "house_number", null: false
+    t.string "building_name"
+    t.string "phone_number"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_destinations_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +43,5 @@ ActiveRecord::Schema.define(version: 2021_01_18_121429) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "destinations", "users"
 end
