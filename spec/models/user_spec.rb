be203ef6acb2fa6c969f3acbb5789d909598bcd3
mk299 <dev+github@clover-dot.net>
2021-01-27 01:50:37 +0900

    test: user、destinationのspecファイル作成

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+describe User do
+  describe '#create' do
+
+    it "nickname、email、password、password_confirmation、first_name、family_name、first_name_kana、family_name_kana、birth_dateがあれば登録できること" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it "nicknameが空では登録できないこと" do
+      user = build(:user, nickname: nil)
+      user.valid?
+      expect(user.errors[:nickname]).to include("を入力してください")
+    end
+
+    it "emailが空では登録できないこと" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it "重複したemailが存在する場合登録できないこと" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
+    end
+
+    it "passwordが空では登録できないこと" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+
+    it "passwordが存在してもpassword_confirmationが空では登録できないこと" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+    end
+
+    it "passwordが7文字以上であれば登録できること" do
+      user = build(:user, password: "0000000", password_confirmation: "0000000")
+      expect(user).to be_valid
+    end
+
+    it "first_nameがないと登録できないこと" do
+      user = build(:user, first_name: nil)
+      user.valid?
+      expect(user.errors[:first_name]).to include("を入力してください")
+    end
+
+    it "family_nameがないと登録できないこと" do
+      user = build(:user, family_name: nil)
+      user.valid?
+      expect(user.errors[:family_name]).to include("を入力してください")
+    end
+
+    it "first_name_kanaがないと登録できないこと" do
+      user = build(:user, first_name_kana: nil)
+      user.valid?
+      expect(user.errors[:first_name_kana]).to include("を入力してください")
+    end
+
+    it "family_name_kanaがないと登録できないこと" do
+      user = build(:user, family_name_kana: nil)
+      user.valid?
+      expect(user.errors[:family_name_kana]).to include("を入力してください")
+    end
+
+    it "birth_dateがないと登録できないこと" do
+      user = build(:user, birth_date: nil)
+      user.valid?
+      expect(user.errors[:birth_date]).to include("を入力してください")
+    end
+
+  end
+end
