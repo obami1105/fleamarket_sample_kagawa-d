df9d32a6b6fdf78333608183101bb58bb4169858
mk299 <dev+github@clover-dot.net>
2021-01-27 01:12:08 +0900

    update: deviseカスタマイズのため参照コントローラ変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'items#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+
+  devise_scope :user do
+    get 'destinations', to: 'users/registrations#new_destination'
+    post 'destinations', to: 'users/registrations#create_destination'
+  end
+
+  root to: 'items#index'
 end
