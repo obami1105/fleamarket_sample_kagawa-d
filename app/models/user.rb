4b699c7bb6d48ed0166c1193c2c117d4f2c6715d
mk299 <dev+github@clover-dot.net>
2021-01-27 01:02:27 +0900

    update: userモデル/バリデーション、アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :birth_date, presence: true
+  validates :password, presence: true, length: { minimum: 7 }
+
+  VALID_FIRSTNAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :first_name, presence: true, format: { with: VALID_FIRSTNAME_REGEX }
+
+  VALID_FAMILYNAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :family_name, presence: true, format: { with: VALID_FAMILYNAME_REGEX }
+
+  VALID_FIRSTNAMEKANA_REGEX = /\A[ぁ-んー－]+\z/
+  validates :first_name_kana, presence: true, format: { with: VALID_FIRSTNAMEKANA_REGEX }
+
+  VALID_FAMILYNAMEKANA_REGEX = /\A[ぁ-んー－]+\z/
+  validates :family_name_kana, presence: true, format: { with: VALID_FAMILYNAMEKANA_REGEX }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+  has_one :destination, dependent: :destroy
 end
