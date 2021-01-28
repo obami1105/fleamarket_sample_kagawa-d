69679385c46eb0ceffc336e08dd01af6e4d82c8e
obami1105 <75107822+obami1105@users.noreply.github.com>
2021-01-29 02:10:40 +0900

    update：商品購入確認ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  get 'purchase', to: 'items#purchase'
-  # root :to => 'items#purchase', as: 'purchase'
-  resources :items, only: [:new,:show]
-  end
   resources :items, only: [:new, :show]
+  get 'purchase', to: 'items#purchase'
 end
