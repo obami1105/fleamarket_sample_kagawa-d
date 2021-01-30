3e968359edf0a74cc20972d6f038d9a096b71108
mk299 <dev+github@clover-dot.net>
2021-01-30 15:31:17 +0900

    update: テストケースの共通処理をbefore句で記述

--- spec/models/destination_spec.rb
+++ spec/models/destination_spec.rb
@@ -1,34 +1,36 @@
 require 'rails_helper'
 describe Destination do
   describe '#create' do
+    before do
+      @destination = build(:destination)
+    end
 
     it "post_code、prefecture_id、city、house_numberがあれば登録できること" do
-      destination = build(:destination)
-      expect(destination).to be_valid
+      expect(@destination).to be_valid
     end
 
     it "post_codeが空では登録できないこと" do
-      destination = build(:destination, post_code: nil)
-      destination.valid?
-      expect(destination.errors[:post_code]).to include("を入力してください")
+      @destination.post_code = nil
+      @destination.valid?
+      expect(@destination.errors[:post_code]).to include("を入力してください")
     end
 
     it "prefecture_idが空では登録できないこと" do
-      destination = build(:destination, prefecture_id: nil)
-      destination.valid?
-      expect(destination.errors[:prefecture_id]).to include("を入力してください")
+      @destination.prefecture_id = nil
+      @destination.valid?
+      expect(@destination.errors[:prefecture_id]).to include("を入力してください")
     end
 
     it "cityが空では登録できないこと" do
-      destination = build(:destination, city: nil)
-      destination.valid?
-      expect(destination.errors[:city]).to include("を入力してください")
+      @destination.city = nil
+      @destination.valid?
+      expect(@destination.errors[:city]).to include("を入力してください")
     end
 
     it "house_numberが空では登録できないこと" do
-      destination = build(:destination, house_number: nil)
-      destination.valid?
-      expect(destination.errors[:house_number]).to include("を入力してください")
+      @destination.house_number = nil
+      @destination.valid?
+      expect(@destination.errors[:house_number]).to include("を入力してください")
     end
 
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,76 +1,117 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
+    before do
+      @user = build(:user)
+    end
 
     it "nickname、email、password、password_confirmation、first_name、family_name、first_name_kana、family_name_kana、birth_dateがあれば登録できること" do
-      user = build(:user)
-      expect(user).to be_valid
+      expect(@user).to be_valid
     end
 
     it "nicknameが空では登録できないこと" do
-      user = build(:user, nickname: nil)
-      user.valid?
-      expect(user.errors[:nickname]).to include("を入力してください")
+      @user.nickname = nil
+      @user.valid?
+      expect(@user.errors[:nickname]).to include("を入力してください")
     end
 
     it "emailが空では登録できないこと" do
-      user = build(:user, email: nil)
-      user.valid?
-      expect(user.errors[:email]).to include("を入力してください")
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors[:email]).to include("を入力してください")
     end
 
     it "重複したemailが存在する場合登録できないこと" do
-      user = create(:user)
-      another_user = build(:user, email: user.email)
+      @user.save
+      another_user = build(:user, email:@user.email)
+      another_user.email = @user.email
       another_user.valid?
       expect(another_user.errors[:email]).to include("はすでに存在します")
     end
 
+    it "emailが空では登録できないこと" do
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors[:email]).to include("を入力してください")
+    end
+
+    it 'emailは@とドメインがなければ登録できないこと' do
+      @user.email = "aaa＠aaa,com"
+      @user.valid?
+      expect(@user.errors[:email]).to include("は不正な値です")
+    end
+
     it "passwordが空では登録できないこと" do
-      user = build(:user, password: nil)
-      user.valid?
-      expect(user.errors[:password]).to include("を入力してください")
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors[:password]).to include("を入力してください")
     end
 
     it "passwordが存在してもpassword_confirmationが空では登録できないこと" do
-      user = build(:user, password_confirmation: "")
-      user.valid?
-      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+      @user.password_confirmation = ""
+      @user.valid?
+      expect(@user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
     end
 
-    it "passwordが7文字以上であれば登録できること" do
-      user = build(:user, password: "0000000", password_confirmation: "0000000")
-      expect(user).to be_valid
+    it "passwordが7文字に満たないときは登録できないこと" do
+      @user.password = "000000"
+      @user.password_confirmation = "000000"
+      @user.valid?
+      expect(@user.errors[:password]).to include("は7文字以上で入力してください")
     end
 
     it "first_nameがないと登録できないこと" do
-      user = build(:user, first_name: nil)
-      user.valid?
-      expect(user.errors[:first_name]).to include("を入力してください")
+      @user.first_name = nil
+      @user.valid?
+      expect(@user.errors[:first_name]).to include("を入力してください")
     end
 
     it "family_nameがないと登録できないこと" do
-      user = build(:user, family_name: nil)
-      user.valid?
-      expect(user.errors[:family_name]).to include("を入力してください")
+      @user.family_name = nil
+      @user.valid?
+      expect(@user.errors[:family_name]).to include("を入力してください")
     end
 
     it "first_name_kanaがないと登録できないこと" do
-      user = build(:user, first_name_kana: nil)
-      user.valid?
-      expect(user.errors[:first_name_kana]).to include("を入力してください")
+      @user.first_name_kana = nil
+      @user.valid?
+      expect(@user.errors[:first_name_kana]).to include("を入力してください")
     end
 
     it "family_name_kanaがないと登録できないこと" do
-      user = build(:user, family_name_kana: nil)
-      user.valid?
-      expect(user.errors[:family_name_kana]).to include("を入力してください")
+      @user.family_name_kana = nil
+      @user.valid?
+      expect(@user.errors[:family_name_kana]).to include("を入力してください")
+    end
+
+    it 'first_nameが全角でなければ登録できないこと' do
+      @user.first_name = "ｱｲｳｴｵ"
+      @user.valid?
+      expect(@user.errors[:first_name]).to include("は不正な値です")
+    end
+
+    it 'family_nameが全角でなければ登録できないこと' do
+      @user.family_name = "ｱｲｳｴｵ"
+      @user.valid?
+      expect(@user.errors[:family_name]).to include("は不正な値です")
+    end
+
+    it 'first_name_kanaが全角ひらがなでなければ登録できないこと' do
+      @user.first_name_kana = "アイウエオ"
+      @user.valid?
+      expect(@user.errors[:first_name_kana]).to include("は不正な値です")
+    end
+
+    it 'family_name_kanaが全角ひらがなでなければ登録できないこと' do
+      @user.family_name_kana = "アイウエオ"
+      @user.valid?
+      expect(@user.errors[:family_name_kana]).to include("は不正な値です")
     end
 
     it "birth_dateがないと登録できないこと" do
-      user = build(:user, birth_date: nil)
-      user.valid?
-      expect(user.errors[:birth_date]).to include("を入力してください")
+      @user.birth_date = nil
+      @user.valid?
+      expect(@user.errors[:birth_date]).to include("を入力してください")
     end
 
   end
