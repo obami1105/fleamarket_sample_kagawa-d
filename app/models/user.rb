0ef5d7f9d7920f499c7a7e8dc5eee394cbc93180
mk299 <dev+github@clover-dot.net>
2021-01-30 15:24:55 +0900

    update: 重複している正規表現パターンをまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,13 @@ class User < ApplicationRecord
   validates :nickname, :birth_date, presence: true
   validates :password, presence: true, length: { minimum: 7 }
 
-  VALID_FIRSTNAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-  validates :first_name, presence: true, format: { with: VALID_FIRSTNAME_REGEX }
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :family_name, presence: true, format: { with: VALID_NAME_REGEX }
 
-  VALID_FAMILYNAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-  validates :family_name, presence: true, format: { with: VALID_FAMILYNAME_REGEX }
-
-  VALID_FIRSTNAMEKANA_REGEX = /\A[ぁ-んー－]+\z/
-  validates :first_name_kana, presence: true, format: { with: VALID_FIRSTNAMEKANA_REGEX }
-
-  VALID_FAMILYNAMEKANA_REGEX = /\A[ぁ-んー－]+\z/
-  validates :family_name_kana, presence: true, format: { with: VALID_FAMILYNAMEKANA_REGEX }
+  VALID_KANA_REGEX = /\A[ぁ-んー－]+\z/
+  validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
+  validates :family_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
