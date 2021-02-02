8da5845ed3d8d3bce1b77fdf64bdd732f5513059
mk299 <dev+github@clover-dot.net>
2021-02-03 00:44:45 +0900

    add: ログアウト後画面のパスをカスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
+    registrations: 'users/registrations'
   }
 
   devise_scope :user do
     get 'destinations', to: 'users/registrations#new_destination'
     post 'destinations', to: 'users/registrations#create_destination'
+    get 'signout', to: 'users#signout'
   end
 
-  root to: 'items#index'
   resources :items, only: [:new, :show]
   get 'purchase', to: 'items#purchase'
+  get 'mypage', to: 'users#show'
+  resources :credit_cards, only: [:index, :new]
+  root 'items#index'
 end
