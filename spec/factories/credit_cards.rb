ad88350ca0924231e2e9fa19620bfe176f79f6be
mk299 <dev+github@clover-dot.net>
2021-02-17 23:11:52 +0900

    test: credit_cards単体テスト（モデル）

--- spec/factories/credit_cards.rb
+++ spec/factories/credit_cards.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :credit_card do
-    
+    user_id       {FactoryBot.create(:user).id}
+    customer_id   {"cus_xxxxxxxxxxxxxxxxxxxxxxxxx"}
+    card_id       {"car_xxxxxxxxxxxxxxxxxxxxxxxxx"}
   end
 end
--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
+describe CreditCard do
+  describe '#pay' do
+    before do
+      @credit_card = build(:credit_card)
+    end
 
-RSpec.describe CreditCard, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "user_id, customer_id, card_idがあれば登録できること" do
+      expect(@credit_card).to be_valid
+    end
+
+    it "user_idがなければ登録できないこと" do
+      @credit_card.user_id = nil
+      @credit_card.valid?
+      expect(@credit_card).to be_invalid
+    end
+
+    it "customer_idがなければ登録できないこと" do
+      @credit_card.customer_id = nil
+      @credit_card.valid?
+      expect(@credit_card).to be_invalid
+    end
+
+    it "card_idがなければできないこと" do
+      @credit_card.card_id = nil
+      @credit_card.valid?
+      expect(@credit_card).to be_invalid
+    end
+
+  end
 end
