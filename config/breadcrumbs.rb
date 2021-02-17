3d3258e8aee52131bfc6a6c1885027517eb79d1f
obami1105 <75107822+obami1105@users.noreply.github.com>
2021-02-17 14:19:37 +0900

    add：パンくず_商品詳細(商品id紐付け)

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -8,30 +8,17 @@ crumb :mypage do
   link "マイページ", mypage_path
   parent :root
 end
-
 # マイページ_子
 crumb :credit_cards do
   link "支払い方法", credit_cards_path
   parent :mypage
 end
 
-# 商品詳細 仮
+# 商品詳細
 crumb :item do
-  link "ごはん", item_path
+  link Item.find(params[:id]).name, item_path
   parent :root
 end
-
-
-# # #商品詳細 差替_リンク名連動
-# crumb :item do
-#   link Item.find(params[:id]).name, item_path
-#   parent :root
-# end
-
-# crumb :item do
-#   link @item.name, item_path
-#   parent :root
-# end
 
 crumb :search do
   link "検索結果", search_items_path
