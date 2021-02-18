0ff18a3674e8b171eb31e8f4df5a38d897284479
omatsu0128 <f1ma2ma3@hotmail.com>
2021-02-18 19:50:28 +0900

    SNSによるユーザー新規登録・パスワード自動生成
    一旦完成
    テスト直前

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ApplicationRecord
       sns.user = user
       sns.save
     end
-    user
+    { user: user, sns: sns}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
+    sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
