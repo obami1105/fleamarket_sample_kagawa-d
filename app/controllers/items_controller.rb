66eff13a9942d509911d17f9c40c3818f566efe4
omatsu0128 <f1ma2ma3@hotmail.com>
2021-02-21 21:54:13 +0900

    最終調整
    １列に３個ずつ表示されるため、合計１２個まで表示するようにしました。
    コメントアウトが残っていたため、さくじょしました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -54,7 +54,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @items = Item.where(user_id: @item.user_id).where.not(id: @item.id).order("created_at DESC").limit(10)
+    @items = Item.where(user_id: @item.user_id).where.not(id: @item.id).order("created_at DESC").limit(12)
   end
 
   def search
